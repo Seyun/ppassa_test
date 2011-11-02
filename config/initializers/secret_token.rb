115baa71e82b3275c6290911b22dff2adc5d361e
Seyun <ppassa@gmail.com>
2011-11-02 22:50:31 +0900

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppassa::Application.config.secret_token = '8965ef2f494a73e4b2de96d3f53df79577f35f6aa947311c72224bc6dd05c7bccab5dcfc08094a32977c36e2b1026d85691aafda1ae537327fc3c0f728551768'
